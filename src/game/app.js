1d022be3bb6076c517a9e2121b2a82eaa850dad3
domfromasquared <dom@asquaredpro.com>
2026-01-10 22:44:19 -0800

    Update app.js

--- src/game/app.js
+++ src/game/app.js
@@ -1,6 +1,5 @@
 // src/game/app.js
-import { computeBankProfile } from "./bankInference.js";
-window.computeBankProfile = computeBankProfile; // devtools calibration
+import "./bankInference.js";
 
 import { ELEMENTS, THESES } from "../../element_schema.js";
 import { getJSON, setJSON, setNum } from "../utils/storage.js";
